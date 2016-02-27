e5ae2837bc70e59aa4bb3db5e0ce9f26ef428449
watilin <matilin.torre@gmail.com>
2016-02-27 13:37:53 +0100

    The Great Cataclysm
    Initial commit
    
    Introduced features:
    * Timers for each Dinoz’ next action
    * Levels shown in Dinoz list
    * Locks to prevent a Dinoz from levelling up

---
+++ dinorpg-assistant.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        DinoRPG Assistant
+// @namespace   fr.kergoz-panic.watilin
+// @description Diverses petites améliorations pour le jeu DinoRPG.
+// @version     1.0.0
+// @include     http://www.dinorpg.com/*
+// @downloadURL https://raw.githubusercontent.com/Watilin/DinoRPG-Assistant/master/dinorpg-assistant.user.js
+// @updateURL   https://raw.githubusercontent.com/Watilin/DinoRPG-Assistant/master/dinorpg-assistant.meta.js
+// @run-at      document-start
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @grant       GM_getResourceText
+// @grant       GM_getResourceURL
+// @resource    style                   style.css
+// @resource    up-locked-small         small_lup_locked.png
+// @resource    up-locked-large         act_levelup_locked.png
+// ==/UserScript==
