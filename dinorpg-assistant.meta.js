feb0c4b050564ef6bab722536eaac9fbe5afbc43
watilin <matilin.torre@gmail.com>
2016-03-01 16:44:50 +0100

    * Updates version number * Removes those silly BOMs * Adds a 32x32 icon * Removes an insidious nobreak space in the CSS

--- dinorpg-assistant.meta.js
+++ dinorpg-assistant.meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name        DinoRPG Assistant
 // @namespace   fr.kergoz-panic.watilin
-// @version     1.0.0
+// @version     1.1.0
 // @description Diverses petites améliorations pour le jeu DinoRPG.
 // @author      Watilin
 // @licence     GNU/GPL 2.0
 //
 // @include     http://www.dinorpg.com/*
 //
+// @icon        icon-rocky.png
+// @icon64      icon-rocky.png
 // @downloadURL https://raw.githubusercontent.com/Watilin/DinoRPG-Assistant/master/dinorpg-assistant.user.js
 // @updateURL   https://raw.githubusercontent.com/Watilin/DinoRPG-Assistant/master/dinorpg-assistant.meta.js
 // @supportURL  https://github.com/Watilin/DinoRPG-Assistant/issues
@@ -24,7 +26,6 @@
 // @grant       GM_getResourceText
 // @grant       GM_getResourceURL
 //
-// @icon        icon-rocky.png
 // @resource    style                   style.css
 // @resource    up-locked-small         small_lup_locked.png
 // @resource    up-locked-large         act_levelup_locked.png
